814b5c4c664b3242ab653a24738b7c9c1d7883c8
chaology <chaopaninchina@gmail.com>
2023-07-11 00:32:15 +0800

    update meta.json and config

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,6 +3,19 @@ export default {
   // project: {
   //   link: 'https://www.fantuyun.com/'
   // },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 泛图云'
+    }
+  },
+  search: {
+    placeholder:"搜索文档内容"
+  },
+  toc: {
+    title:"该页面"
+  },
+  editLink:false,
+  feedback:false,
   footer: {
     text: 'Copyright © 2023 泛图云',
   }
