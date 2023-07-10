673aa0000c8c18790efc7ba56d06825a5e2bb5d9
chaology <chaopaninchina@gmail.com>
2023-07-10 22:43:32 +0800

    Deploy

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,8 @@
 export default {
-  logo: <span>泛图云</span>,
-  project: {
-    link: 'https://www.fantuyun.com/'
-  },
+  logo: <strong>泛图云</strong>,
+  // project: {
+  //   link: 'https://www.fantuyun.com/'
+  // },
   footer: {
     text: 'Copyright © 2023 泛图云',
   }
