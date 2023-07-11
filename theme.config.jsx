5a8a035e38a1a738607f1783fcee8f7f80891152
chaology <chaopaninchina@gmail.com>
2023-07-11 11:04:36 +0800

    change meta

--- theme.config.jsx
+++ theme.config.jsx
@@ -16,6 +16,15 @@ export default {
   },
   editLink:false,
   feedback:false,
+  themeSwitch: {
+    useOptions() {
+      return {
+        light: '浅色',
+        dark: '深色',
+        system: '系统主题'
+      }
+    }
+  },
   footer: {
     text: 'Copyright © 2023 泛图云',
   }
